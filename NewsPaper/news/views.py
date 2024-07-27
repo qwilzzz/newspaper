9705b4a62fc3c2643f76bbc5e4088d84204d6825
qwilzzz <onceqwilzz@gmail.com>
2024-07-28 08:30:34 +1000

    views and templates for '/news/' and '/news/{n}'

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -4,10 +4,10 @@ from .models import Post
 class PostNList(ListView):
     model = Post
     template_name = 'news/posts_news.html'
-    context_object_name = 'posts_news'
+    context_object_name = 'posts'
 
 
 class PostNDetail(DetailView):
     model = Post
     template_name = 'news/post_news.html'
-    context_object_name = 'post_news'
+    context_object_name = 'post'
