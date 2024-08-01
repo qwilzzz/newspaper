d2515933d45c6291a1758489e7d7f49143a1a9e8
qwilzzz <onceqwilzz@gmail.com>
2024-08-01 13:02:02 +1000

    added pagination, filters and /news/search page

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostNList, PostNDetail  # импортируем наше представление
+from .views import PostNList, PostNDetail, Posts, Search  # импортируем наше представление
 
 urlpatterns = [
     # path -- означает путь. В данном случае путь ко всем товарам у нас останется пустым, позже станет ясно почему
     path('', PostNList.as_view()),
     path('<int:pk>/', PostNDetail.as_view()),
+    path('posts/', Posts.as_view()),
+    path('search/', Search.as_view()),
 ]
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,13 +1,47 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django.views import View
+from django.core.paginator import Paginator
+from .filters import PostFilter
 from .models import Post
 
 class PostNList(ListView):
     model = Post
     template_name = 'news/posts_news.html'
     context_object_name = 'posts'
-
+    ordering = ['-creation_date']
+    filter_class = PostFilter
+    paginate_by = 10
 
 class PostNDetail(DetailView):
     model = Post
     template_name = 'news/post_news.html'
     context_object_name = 'post'
+
+class Posts(View):
+    def get(self, request):
+        posts = Post.objects.order_by('-creation_date')
+        paginator = Paginator(posts, 1)
+        posts = paginator.get_page(request.GET.get('page', 1))
+        data = {
+            'posts': posts,
+                }
+        return render(request, 'news/posts_news.html', data)
+
+class Search(ListView):
+    model = Post
+    template_name = 'news/search.html'
+    context_object_name = 'search'
+    ordering = ['-creation_date']
+    filter_class = PostFilter
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filter = self.filter_class(self.request.GET, queryset=queryset)
+        return self.filter.qs.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = self.filter
+        return context
